d7c994892bccdebad216ef8d2d4f81a9b182eff1
RoySamson-stack <rsam9009@gmail.com>
2021-10-31 12:07:11 +0300

    adding the preloader to the site

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-// $(document).ready(function(){
-//   $(".link-btn").click(function(){
-//     $(".main-title").toggle();
-//     $(".title").toggle();
-//     $(".contact").toggle();
-//     $(".image_me").toggle();
-//     $("#projects").toggle();
-//   })
-// })
+$(document).ready(function(){
+
+})
