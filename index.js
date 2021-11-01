f77255c770e3f02396310b7d97b9b73ea1d8a566
RoySamson-stack <rsam9009@gmail.com>
2021-11-01 22:18:06 +0300

    making the page delay for the  preloader to have more time

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
+  $(".main").delay(7000);
   $('#loading_wrap').delay(1000).fadeOut(300,function(){
     setTimeout(function(){
       $('#loading_wrap').remove()
     } ,100);
-
 });
 });
