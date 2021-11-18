958ca289a62fb38704528a41de6588c368fa734e
RoySamson-stack <rsam9009@gmail.com>
2021-11-18 22:17:13 +0300

    adding the langues cubes

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+
 $(document).ready(function(){
  
   $('#loading_wrap').delay(1000).fadeOut(300,function(){
@@ -5,4 +7,10 @@ $(document).ready(function(){
       $('#loading_wrap').remove()
     } ,100);
 });
+const switchElement = document.querySelector('.switch');
+
+switchElement.addEventListener('click', () => {
+document.body.classList.toggle('dark');
 });
+});
+
