928e2879ecaffd5c242368abc46b82e3008bde3a
RoySamson-stack <rsam9009@gmail.com>
2021-11-21 23:01:31 +0300

    adding the hover effect for the project cards

--- index.js
+++ index.js
@@ -12,5 +12,13 @@ const switchElement = document.querySelector('.switch');
 switchElement.addEventListener('click', () => {
 document.body.classList.toggle('dark');
 });
+
+$(".tile").moouseenter(function(){
+  $(".tile-content").show();
+})
+$(".tile").mouseleave(function(){
+  $(".tile-content").hide(); 
+ });
+
 });
 
